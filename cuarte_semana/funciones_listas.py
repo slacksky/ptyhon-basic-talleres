ea3eb33e5f2534eaa7a0ad6f0044b9babf3985d9
slacksky <slacksky@gmail.com>
2021-05-31 17:49:03 -0500

    2nd  workshop

---
+++ cuarte_semana/funciones_listas.py
@@ -0,0 +1,27 @@
+""" Modulo Listas
+    Funciones para practicas con listas
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+# Definición de Funciones
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+def suma_acumulativa(lista_numeros):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def traductor_pig_latin(lista_palabras):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def identificador_frutas(lista_frutas):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def son_palindromos(frase_uno, frase_dos):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
