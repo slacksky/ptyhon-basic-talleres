6a9db7fa074be313ba035ba4b00c2627f529ebcd
Jviv <slacksky@gmail.com>
2021-05-31 16:28:38 -0500

    workshop 1 complte for 4th week

--- cuarte_semana/funciones_ciclos.py
+++ cuarte_semana/funciones_ciclos.py
@@ -21,10 +21,14 @@ def simulador_caida_libre(altura):
 
 def generador_generaciones(generacion):
   ##in: generacion, mid: total_p, out: total_p
-  total_p=1
+  total_p=0
   gen=range(generacion)
   for x in gen:
-    total_p+= 2**x
+    print("generacion", x+1)##+1 para mejor entendimiento de las generaciones
+    anc=2**x
+    print("persona(s) en esta generacion: ", anc) 
+    total_p+=anc 
+    print("total acumulado, ",total_p," para la generacion ",x+1)
   return total_p
   """en resumida cuentas seria sumatorias con elevacion de 2 por cada genracion"""
   
@@ -32,13 +36,54 @@ def generador_generaciones(generacion):
 def constructor_triangulos(pisos):
   #TODO Comentar código
   #TODO Implementar la función
+  numero=1
+  for i in range(1,pisos+1):
+    for j in range(1,i+1):
+      print(numero, end= ' ')
+      numero+=1
+    print()
 
   """numero de piso triangulares revisar con fibonachi para fors anidados"""
-  return "No implementada aún"
+  #return "No implementada aún"
 
 def constructor_tableros(longitud):
   #TODO Comentar código
   #TODO Implementar la función
 
-  ##usar un template para cada rectaincugna y  pone for anidados para crear la cuadricula
+  b=""
+  w=""
+  b,w=row_builder(longitud)
+  board_builder(longitud,b,w)
+
+
+def tile_third(color):
+  if color == 0:
+    return "***"
+  else:
+    return "   "
+
+def full_tile(t3):
+  for i in range(3):
+    print(t3)
+
+def row_builder(longitud):
+  tileb=""
+  tilew=""
+  for i in range(longitud):
+    tileb+=tile_third(i%2)
+  
+  if longitud>1:
+    for i in range(longitud):
+      tilew+=tile_third((i+1)%2)
+  return tileb,tilew
+
+def board_builder(longitud, black, white):
+  for i in range(longitud):
+    if i%2==0:
+      full_tile(black)
+    else:
+      full_tile(white)
+
+
+  ##usar un template para cada cuadrado y poner for anidados para crear la cuadricula
   return "No implementada aún"
--- cuarte_semana/taller_1.py
+++ cuarte_semana/taller_1.py
@@ -12,17 +12,31 @@ import funciones_ciclos as f
 
 
 def main():
-    #TODO: 2 a 4 funciones
+    #TODO: menu
+    print("## Bienvenido, a multi-herramienta T ##")
+    print("## 1->simulacion caida libre    ##")
+    print("## 2->calculo generacional      ##")
+    print("## 3->triangulo de enteros      ##")
+    print("## 4->tableros custom de ajedres##")
 
-    
-    #dist=float(input("ingrese la altura: "))
-    #f.simulador_caida_libre(dist)
-    generacion=int(input("cuantas generaciones hacia atras quieres visualizar?:"))
-    total_personas=f.generador_generaciones(generacion)
-    print("en estas ",generacion," generaciones, han habido, ",total_personas," personas ")
+    menu=int(input("##porfavor, elegir una  opcion (1,2,3,4)## \n"))
 
-
-
+    if menu == 1:
+        dist=float(input("ingrese la altura en metros: "))
+        f.simulador_caida_libre(dist)
+    elif menu == 2:
+        generacion=int(input("cuantas generaciones hacia atras quieres visualizar?:"))
+        total_personas=f.generador_generaciones(generacion)
+        print("en estas ",generacion," generaciones, han habido, ",total_personas," personas ")
+    elif menu == 3:
+        pisos=int(input("con cuantos pisos quieres el triangulo? "))
+        f.constructor_triangulos(pisos)
+    elif menu == 4:
+        cuadros=int(input("cuantos cuadrosXcuadros para el tablero?"))
+        f.constructor_tableros(cuadros)
+    else:
+        print("## porfavor elige uno de los numeros  del 1 al 4##")
+    print("trabajo completado, Cerrando ... bye")
 
 
 
